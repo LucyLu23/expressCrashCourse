c254e919ba3426240f0a69370bc00e2b3d76c0a6
LucyLu23 <114943897+LucyLu23@users.noreply.github.com>
2022-11-04 00:02:45 -0600

    updateexpresscrashcourse

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require ('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+//const moment = require('momnet');
+const logger = require('./middleware/logger')
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send(<h1>'Hello World'</h1>);
-});
+const { allowedNodeEnvironmentFlags } = require('process');
 
-const PORT = process.env.PORT || 5000;
+
+
+
+//init middleware
+// app.use(logger); 
+
+//handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//homwpage route
+app.get('/',(req, res) => res.render('index',{
+   title: 'Member App',
+   members
+}));
+//set static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+/* app.get('/', (req, res) => {
+    res.sendFile(path.join (__dirname, 'public','index.html'));
+}); */
+
+//Members API Routes
+app.use('/api/members', require('./routes/api/members'));
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=>console.log(`Sever started on port ${PORT}`));
 
